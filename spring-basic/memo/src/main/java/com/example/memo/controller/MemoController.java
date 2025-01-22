24779b1360f0edb28075786b7faf07019afcadca
subbni <slfkak@naver.com>
2025-01-22 16:49:56 +0900

    feat: 메모 프로젝트 마무리 실습

--- spring-basic/memo/src/main/java/com/example/memo/controller/MemoController.java
+++ spring-basic/memo/src/main/java/com/example/memo/controller/MemoController.java
@@ -3,11 +3,15 @@ package com.example.memo.controller;
 import com.example.memo.dto.MemoRequestDto;
 import com.example.memo.dto.MemoResponseDto;
 import com.example.memo.entity.Memo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/memos")
@@ -15,7 +19,7 @@ public class MemoController {
     private final Map<Long, Memo> memoList = new HashMap<>();
 
     @PostMapping
-    public MemoResponseDto createMemo(
+    public ResponseEntity<MemoResponseDto> createMemo(
             @RequestBody MemoRequestDto requestDto
     ) {
         Long memoId = memoList.isEmpty() ? 1 : Collections.max(memoList.keySet())+1;
@@ -23,31 +27,72 @@ public class MemoController {
         Memo memo = new Memo(memoId, requestDto.getTitle(), requestDto.getContents());
 
         memoList.put(memoId, memo);
-        return new MemoResponseDto(memo);
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<MemoResponseDto> findAll() {
+        return memoList.values().stream().map(MemoResponseDto::new).collect(Collectors.toList());
     }
 
     @GetMapping("/{id}")
-    public MemoResponseDto findMemoById(
+    public ResponseEntity<MemoResponseDto> findMemoById(
             @PathVariable  Long id
     ) {
+        if(!memoList.containsKey(id)) {
+            // exception 처리
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
         Memo memo = memoList.get(id);
-        return new MemoResponseDto(memo);
+        return new ResponseEntity<MemoResponseDto>(new MemoResponseDto(memo), HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
-    public MemoResponseDto updateMemoById(
+    public ResponseEntity<MemoResponseDto> updateMemo(
         @PathVariable Long id,
         @RequestBody MemoRequestDto requestDto
     ) {
+        if(!memoList.containsKey(id)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        if(requestDto.getTitle()==null || requestDto.getContents()==null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
         Memo memo = memoList.get(id);
         memo.update(requestDto);
-        return new MemoResponseDto(memo);
+        return new ResponseEntity<MemoResponseDto>(new MemoResponseDto(memo), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MemoResponseDto> updateMemoTitle(
+            @PathVariable Long id,
+            @RequestBody MemoRequestDto requestDto
+    ) {
+        if(!memoList.containsKey(id)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        if(requestDto.getTitle()==null || requestDto.getContents()==null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        Memo memo = memoList.get(id);
+        memo.updateTitle(requestDto);
+        return new ResponseEntity<MemoResponseDto>(new MemoResponseDto(memo), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteMemo(
+    public ResponseEntity<Void> deleteMemo(
             @PathVariable Long id
     ) {
+        if(!memoList.containsKey(id)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
         memoList.remove(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
